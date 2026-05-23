1427b431d3e0a3a0760b7ce3fb6bfc76d00be442
sjbrenchley89 <s.j.brenchley89@gmail.com>
2026-05-23 21:33:17 +1000

    feat: add global CSS, main entry, and App shell

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import LoadingScreen from './components/LoadingScreen'
+import Hero from './components/Hero'
+import SelectedWorks from './components/SelectedWorks'
+import Journal from './components/Journal'
+import Explorations from './components/Explorations'
+import Stats from './components/Stats'
+import Contact from './components/Contact'
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  return (
+    <main className="bg-bg text-text-primary font-body">
+      {isLoading && <LoadingScreen onComplete={() => setIsLoading(false)} />}
+      <Hero />
+      <SelectedWorks />
+      <Journal />
+      <Explorations />
+      <Stats />
+      <Contact />
+    </main>
+  )
+}
